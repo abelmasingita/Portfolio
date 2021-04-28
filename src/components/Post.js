e471b3abf3f00b66d12028a1abde51e879198e9d
Masingita <hlongwaniab@gmail.com>
2021-04-28 07:08:39 +0200

    favicon changed

--- src/components/Post.js
+++ src/components/Post.js
@@ -27,7 +27,7 @@ import sanityClient from '../client'
     },[])
 
       return (
-          <main className='min-h-screen p-12'>
+          <main className='bg-gray-100 min-h-screen p-12'>
                 <section className='container mx-auto'>
                     <h1 className='text-5xl flex justify-center cursive'>Blog Posts Page</h1>
                     <h2 className='text-lg text-gray-600 flex justify-center mb-12'>Welcome to my page of blog posts</h2>
