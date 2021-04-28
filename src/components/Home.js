72fb6468f29abfae1a0503e0be4bac5b59447237
Masingita <hlongwaniab@gmail.com>
2021-04-28 06:01:35 +0200

    porfolio done

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import image from  '../monstera-leaf.jpg'
+
+const Home = () => {
+    return (
+            <main>
+                    <img  src={image} alt="monstera-leaf" className="absolute object-cover w-full h-full"/>
+                    <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+                        <h1 className='text-sm text-red-100 font-bold cursive leading-none lg:leading-snug home-name'>
+                            Ahee. I'm abel masingita.
+                        </h1>
+                    </section>
+            </main>
+    )
+}
+
+export default Home
