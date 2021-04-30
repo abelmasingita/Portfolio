c9165d7fa4200ed3ece9ceaee9581e0dbd1db01e
Masingita <masinge322@gmail.com>
2021-04-30 18:02:55 +0200

    banner again

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import image from  '../monstera-leaf.jpg'
 import './Footer.css'
+import "../bootstrap.min.css"
 
 const Home = () => {
     return (
-            <main>
-                    <img  src={image} alt="monstera-leaf" className="absolute object-cover w-full h-full"/>
+            <main style={{backgroundColor: "#1B5895"}}>
+                    <img  src={image} alt="monstera-leaf" className="absolute object-cover w-100 h-100"/>
                     <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
                     <div className="banner-style lg:mt-80 ">
-                        <h3 className="ban-text lg:font-bold lg:text-6xl lg:text-red-100">I'm <span>Abel Masingita</span></h3>
+                        <h3 className="ban-text lg:font-bold lg:text-6xl text-3xl lg:text-red-100">I'm <span>Abel Masingita</span></h3>
                         <div className="content">
                         <h2 className="ban-text">
                             <span className="logo-tag"></span>
-                            <span id="tag-content" className="lg:font-bold lg:text-2xl lg:text-red-100"> Software Engineer</span>
-                            <span className="text-cursor">!</span>
+                            <span id="tag-content" className="lg:font-bold text-1xl lg:text-2xl lg:text-red-100"> Software Engineer</span>
+                            <span className="text-cursor lg:font-bold lg:text-2xl lg:text-red-100">!</span>
                             <span className="logo-tag"></span>
                         </h2>
                         </div>
